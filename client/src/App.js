046306e6cc75c894bd63f2d6d8017abebb5fdd61
Nguyen Hoang Viet <NgHViet@Nguyens-MacBook-Pro.local>
2020-03-23 13:35:24 +0700

    A

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Gate from './Gate';
+import Main from './Main';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id : localStorage.getItem('id'),
+            name : localStorage.getItem('name')
+        }
+    }
+
+
+
+    changeState = (id,name) => {
+        if(id != null) this.setState({id : id,name:name});
+        localStorage.setItem('id',id);
+        localStorage.setItem('name',name);
+    }
+
+    logOut = () => {
+        localStorage.removeItem('id');
+        localStorage.removeItem('name');
+        this.setState({id:null,name:null});
+    }
+
+    render() {
+        if(this.state.id != null) {
+            return(
+                <Main userID = {this.state.id} name = {this.state.name} logOut = {this.logOut}/>
+            );
+        }
+        else {
+            return (
+                <Gate changeState = {this.changeState} />
+            );
+        }
+    }
 }
 
 export default App;
